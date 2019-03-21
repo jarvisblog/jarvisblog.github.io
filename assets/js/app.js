83a01da3fcc704cf63be24c7370bbd76f7a4e936
jarvisblog <jarvisblog@outlook.com>
2019-03-21 16:05:18 +0200

    asd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
-if(window.location.href=="http://jarvisblog.github.io/")
-{
-$("#sidebar").css({width:'100%'});
-$("#btnblog").click(function(){
-$("#sidebar").animate({width:'33.3333%'},'slow');
-});
-}
+  console.log( window.location.href );
+
+  if(window.location.href=="https://chaussures2016.com/") {
+    $("#sidebar").css({width:'100%'});
+
+    $("#btnblog").click(function(){
+      $("#sidebar").animate({width:'33.3333%'},'slow');
+    });
+  }
 });
